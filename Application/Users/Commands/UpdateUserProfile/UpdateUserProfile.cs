399e1dfb07ec00e98a77881aac05e7937db73f84
DeniZhekova <denitsa.zhrkova97@gmail.com>
2019-12-03 16:08:05 +0100

    update profile additions

--- Application/Users/Commands/UpdateUserProfile/UpdateUserProfile.cs
+++ Application/Users/Commands/UpdateUserProfile/UpdateUserProfile.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Entities;
 using MediatR;
+using System;
 
 namespace Application.Users.Commands.UpdateProfile
 {
@@ -9,6 +10,20 @@ namespace Application.Users.Commands.UpdateProfile
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
         public string PhoneNumber { get; set; }
+        public string Introduction { get; set; }
+        public string Interests { get; set; }
+        public string AnimalName { get; set; }
+
+        public string AnimalType { get; set; }
+
+        public string AnimalBreed { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public DateTime AnimalDateOfBirth { get; set; }
     }
 }
